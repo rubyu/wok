bc21bfcfd6e3c200bc3d74fc548b0521ed0b18d2
rubyu <ruby.u.g@gmail.com>
2014-07-18 07:37:54 +0900

    :white_check_mark: Add implicit conversion Seq[String] to Process.

--- src/main/scala/wok/process/Helpers.scala
+++ src/main/scala/wok/process/Helpers.scala
@@ -5,6 +5,8 @@ import wok.core.Helpers.{EscapedString, QuotedString}
 
 
 object Helpers {
+  implicit def seq2process(seq: Seq[String]) = new Process(seq)
+
   implicit def result2out(r: Result) = r.out
 
   implicit class ProcessArgumentString(val s: String) extends AnyVal {
--- src/test/scala/wok/process/ProcessTest.scala
+++ src/test/scala/wok/process/ProcessTest.scala
@@ -8,54 +8,54 @@ import Helpers._
 class ProcessTest extends SpecificationWithJUnit {
   "Process.exec" should {
     "throw IllegalArgumentException when commandStrings is '|'" in {
-      new Process(Seq("|"))
+      Seq("|")
         .exec must throwAn[IllegalArgumentException]
     }
 
     "throw IllegalArgumentException when commandStrings starts with '|'" in {
-      new Process(Seq("|", "a"))
+      Seq("|", "a")
         .exec must throwAn[IllegalArgumentException]
     }
 
     "throw IllegalArgumentException when commandStrings ends with '|'" in {
-      new Process(Seq("a", "|"))
+      Seq("a", "|")
         .exec must throwAn[IllegalArgumentException]
     }
 
     "throw IllegalArgumentException when commandStrings is empty" in {
-      new Process(Seq())
+      Seq()
         .exec must throwAn[IllegalArgumentException]
     }
 
     "call a program" in {
-      new Process(Seq("echo", "a"))
+      Seq("echo", "a")
         .exec.string mustEqual "a\n"
     }
 
     "call a program with arguments" in {
-      new Process(Seq("cat", "./src/test/scala/wok/process/resources/angel.txt"))
+      Seq("cat", "./src/test/scala/wok/process/resources/angel.txt")
         .exec.string mustEqual "angel\r\néindʒəl\r\n"
     }
 
     "call a program with unicode encoded arguments" in {
-      new Process(Seq("cat", "./src/test/scala/wok/process/resources/angel.txt",
-        "|", "grep", "éindʒəl"))
+      Seq("cat", "./src/test/scala/wok/process/resources/angel.txt",
+        "|", "grep", "éindʒəl")
         .exec.string mustEqual "éindʒəl\n"
     }
 
     "call a program with standard input" in {
-      new Process(Seq("cat", "./src/test/scala/wok/process/resources/angel.txt", "-"))
+      Seq("cat", "./src/test/scala/wok/process/resources/angel.txt", "-")
         .exec("stdin-data").string mustEqual "angel\r\néindʒəl\r\nstdin-data"
     }
 
     "call a program with unicode encoded standard input" in {
-      new Process(Seq("cat", "./src/test/scala/wok/process/resources/angel.txt", "-"))
+      Seq("cat", "./src/test/scala/wok/process/resources/angel.txt", "-")
         .exec("éindʒəl").string mustEqual "angel\r\néindʒəl\r\néindʒəl"
     }
 
     "connect programs" in {
-      new Process(Seq("cat", "./src/test/scala/wok/process/resources/angel.txt",
-        "|", "grep", "an"))
+      Seq("cat", "./src/test/scala/wok/process/resources/angel.txt",
+        "|", "grep", "an")
         .exec.string mustEqual "angel\n"
     }
   }

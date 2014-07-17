dbd9c26aa0572ff981f7d0ca0d8685175bb67479
rubyu <ruby.u.g@gmail.com>
2014-07-18 07:28:21 +0900

    :white_check_mark: Add String.escape() for Process's arguments.

--- src/main/scala/wok/process/Helpers.scala
+++ src/main/scala/wok/process/Helpers.scala
@@ -1,8 +1,16 @@
 
 package wok.process
 
+import wok.core.Helpers.{EscapedString, QuotedString}
+
 
 object Helpers {
   implicit def result2out(r: Result) = r.out
+
+  implicit class ProcessArgumentString(val s: String) extends AnyVal {
+    def escaped: String = {
+      new EscapedString(s).escaped('\\', '"').quoted('"')
+    }
+  }
 }
 

fe027005e16036ffc23ec11820396af4955dc0b2
rubyu <ruby.u.g@gmail.com>
2014-07-22 11:13:02 +0900

    :white_check_mark: Add tests that Reader should reflect FS, RS, FQ to Iterator Rreader.open() returned.

--- src/test/scala/wok/csv/ReaderTest.scala
+++ src/test/scala/wok/csv/ReaderTest.scala
@@ -45,5 +45,48 @@ class ReaderTest extends SpecificationWithJUnit {
       result.toList mustEqual List(
         Row(0, List("a1", "a2\nb1", "b2"), List("\t", "\t"), "", "a1\ta2\nb1\tb2"))
     }
+
+    "be changed FS from inside Iterator" in {
+      val reader = Reader()
+        reader
+        .FS("""\t""".r)
+        .RS("\n")
+        .FQ(Quote() None())
+        .open(new java.io.StringReader("a1\ta2\nb1 b2"))
+        .map { row => reader.FS(" "); row }
+        .toList mustEqual List(
+          Row(0, List("a1", "a2"), Nil, "\n", "a1\ta2\n"),
+          Row(1, List("b1", "b2"), Nil, "", "b1 b2")
+        )
+    }
+
+    "be changed RS from inside Iterator" in {
+      val reader = Reader()
+      reader
+        .FS("""\t""".r)
+        .RS("\n")
+        .FQ(Quote() None())
+        .open(new java.io.StringReader("a\nb c"))
+        .map { row => reader.RS(" "); row }
+        .toList mustEqual List(
+        Row(0, List("a"), Nil, "\n", "a\n"),
+        Row(1, List("b"), Nil, " ", "b "),
+        Row(2, List("c"), Nil, "", "c")
+      )
+    }
+
+    "be changed FQ from inside Iterator" in {
+      val reader = Reader()
+      reader
+        .FS("""\t""".r)
+        .RS("\n")
+        .FQ(Quote() None())
+        .open(new java.io.StringReader("\"a\"\n\"b\""))
+        .map { row => reader.FQ(Quote() Min()); row }
+        .toList mustEqual List(
+        Row(0, List("\"a\""), Nil, "\n", "\"a\"\n"),
+        Row(1, List("b"), Nil, "", "\"b\"")
+      )
+    }
   }
 }

4b30cde1a71c0f6abdf500b61bb583d59f45d812
rubyu <ruby.u.g@gmail.com>
2014-08-11 20:40:59 +0900

    Ignore ClassNotFoundException threw by DynamicClass.create() in Main.main

--- src/main/scala/wok/core/Main.scala
+++ src/main/scala/wok/core/Main.scala
@@ -1,14 +1,17 @@
 package wok.core
 
-import wok.reflect.DynamicCompiler
+import wok.reflect.{AbstractWok, DynamicCompiler}
 
 
 object Main {
   def main(args: Array[String]) {
     val cli = CliOption.parse(args.toList)
-    DynamicCompiler
-      .compile(cli.before, cli.process, cli.after)
-      .create(cli.arg)
-      .runScript()
+    val Wok = DynamicCompiler.compile(cli.before, cli.process, cli.after)
+    val wok: AbstractWok =
+      try Wok.create(cli.arg)
+      catch {
+        case e: ClassNotFoundException => System.exit(1); throw e
+      }
+    wok.runScript()
   }
 }

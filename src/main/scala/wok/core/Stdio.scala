0dd6d0fc01d3ec2d6b1fa76757e4e23e3abf7c67
rubyu <ruby.u.g@gmail.com>
2014-08-16 04:07:43 +0900

    :white_check_mark: :bug: Fix a bug Stdio.in/out/err was closed by CloseableOpenedResource.closeAction

--- src/main/scala/wok/core/Stdio.scala
+++ src/main/scala/wok/core/Stdio.scala
@@ -9,15 +9,37 @@ import scalax.io.Resource
 
 
 object Stdio {
-  private val outVar = new DynamicVariable[OutputStreamResource[OutputStream]](Resource.fromOutputStream(java.lang.System.out))
-  private val errVar = new DynamicVariable[OutputStreamResource[OutputStream]](Resource.fromOutputStream(java.lang.System.err))
-  private val inVar = new DynamicVariable[InputStreamResource[InputStream]](Resource.fromInputStream(java.lang.System.in))
+  private object CloseGuard {
+    def apply(in: InputStream) = new CloseGuardInputStream(in)
+    def apply(out: OutputStream) = new CloseGuardOutputStream(out)
+
+    class CloseGuardInputStream(in: InputStream) extends InputStream {
+      def read(): Int = in.read()
+    }
+    class CloseGuardOutputStream(out: OutputStream) extends OutputStream {
+      def write(b: Int) = out.write(b)
+      override def flush() = out.flush()
+    }
+  }
+
+  private val outVar =
+    new DynamicVariable[OutputStreamResource[OutputStream]](
+      Resource.fromOutputStream(CloseGuard(java.lang.System.out)))
+  private val errVar =
+    new DynamicVariable[OutputStreamResource[OutputStream]](
+      Resource.fromOutputStream(CloseGuard(java.lang.System.err)))
+  private val inVar =
+    new DynamicVariable[InputStreamResource[InputStream]](
+      Resource.fromInputStream(CloseGuard(java.lang.System.in)))
 
   def out = outVar.value
   def err = errVar.value
   def in = inVar.value
 
-  def withOut[T](out: OutputStream)(thunk: =>T): T = outVar.withValue(Resource.fromOutputStream(out))(thunk)
-  def withErr[T](err: OutputStream)(thunk: =>T): T = errVar.withValue(Resource.fromOutputStream(err))(thunk)
-  def withIn[T](in: InputStream)(thunk: =>T): T = inVar.withValue(Resource.fromInputStream(in))(thunk)
+  def withOut[T](out: OutputStream)(thunk: =>T): T =
+    outVar.withValue(Resource.fromOutputStream(CloseGuard(out)))(thunk)
+  def withErr[T](err: OutputStream)(thunk: =>T): T =
+    errVar.withValue(Resource.fromOutputStream(CloseGuard(err)))(thunk)
+  def withIn[T](in: InputStream)(thunk: =>T): T =
+    inVar.withValue(Resource.fromInputStream(CloseGuard(in)))(thunk)
 }

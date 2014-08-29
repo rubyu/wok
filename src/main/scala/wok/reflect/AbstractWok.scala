be0342445f7375397fd0e0a648e27a1b18ede62a
rubyu <ruby.u.g@gmail.com>
2014-08-30 00:22:53 +0900

    :lipstick: Refactoring ThreadSafeExecutor

--- src/main/scala/wok/reflect/AbstractWok.scala
+++ src/main/scala/wok/reflect/AbstractWok.scala
@@ -154,27 +154,26 @@ trait AbstractWok {
   }
 
   class ThreadSafeExecutor extends ForkJoinPool {
+    private val threadClass = classOf[Thread]
+    private val threadLocalClass = classOf[ThreadLocal[_]]
+    private val threadLocalMapClass = ClassLoader.getSystemClassLoader.loadClass("java.lang.ThreadLocal$ThreadLocalMap")
+
+    private val threadLocals = threadClass.getDeclaredField("threadLocals")
+    threadLocals.setAccessible(true)
+
+    private val inheritableThreadLocals = threadClass.getDeclaredField("inheritableThreadLocals")
+    inheritableThreadLocals.setAccessible(true)
+
+    private val createInheritedMap = threadLocalClass.getDeclaredMethod("createInheritedMap", threadLocalMapClass)
+    createInheritedMap.setAccessible(true)
+
     override def execute(task: Runnable) {
       val parent = Thread.currentThread()
       super.execute(new Runnable {
         override def run() {
           val current = Thread.currentThread()
-          val threadClass = classOf[Thread]
-          val threadLocalClass = classOf[ThreadLocal[_]]
-          val threadLocalMapClass = ClassLoader.getSystemClassLoader.loadClass("java.lang.ThreadLocal$ThreadLocalMap")
-
-          val threadLocals = threadClass.getDeclaredField("threadLocals")
-          threadLocals.setAccessible(true)
-
-          val inheritableThreadLocals = threadClass.getDeclaredField("inheritableThreadLocals")
-          inheritableThreadLocals.setAccessible(true)
-
-          val createInheritedMap = threadLocalClass.getDeclaredMethod("createInheritedMap", threadLocalMapClass)
-          createInheritedMap.setAccessible(true)
-
           // initialize threadLocals
           threadLocals.set(current, null)
-
           // initialize inheritableThreadLocals
           val parentMap = inheritableThreadLocals.get(parent)
           val currentMap = createInheritedMap.invoke(current, parentMap)
